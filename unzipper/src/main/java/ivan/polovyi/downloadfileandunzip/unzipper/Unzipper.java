bab5fe4af433b22491814ba5d905d089abd6a91f
ivan.polovyi <polovyivan@yahoo.com>
2019-01-28 22:58:14 -0200

    First commit

---
+++ unzipper/src/main/java/ivan/polovyi/downloadfileandunzip/unzipper/Unzipper.java
@@ -0,0 +1,80 @@
+package ivan.polovyi.downloadfileandunzip.unzipper;
+package ivan.polovyi.downloadfileandunzip.unzipper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class Unzipper {
+
+	private static String fileName = "";// final file name
+
+	public static void main(String[] args) {
+
+		String urlString = "";// Download from this URL
+		String destination = ""; // download to this directory
+		String destDir = "";// Extract to this directory
+
+		downloadFileFromURL(urlString, destination);
+		unzip(destination, destDir);
+	}
+
+	private static void unzip(String zipFilePath, String destDir) {
+		File dir = new File(destDir);
+		// create output directory if it doesn't exist
+		if (!dir.exists())
+			dir.mkdirs();
+		FileInputStream fis;
+		// buffer for read and write data to file
+		byte[] buffer = new byte[1024];
+		try {
+			fis = new FileInputStream(zipFilePath);
+			ZipInputStream zis = new ZipInputStream(fis);
+			ZipEntry ze = zis.getNextEntry();
+			while (ze != null) {
+
+				File newFile = new File(destDir + File.separator + fileName);
+				System.out.println("Unzipping to " + newFile.getAbsolutePath());
+				// create directories for sub directories in zip
+				new File(newFile.getParent()).mkdirs();
+				FileOutputStream fos = new FileOutputStream(newFile);
+				int len;
+				while ((len = zis.read(buffer)) > 0) {
+					fos.write(buffer, 0, len);
+				}
+				fos.close();
+				// close this ZipEntry
+				zis.closeEntry();
+				ze = zis.getNextEntry();
+			}
+			// close last ZipEntry
+			zis.closeEntry();
+			zis.close();
+			fis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void downloadFileFromURL(String urlString, String destination) {
+		try {
+			URL website = new URL(urlString);
+			ReadableByteChannel rbc;
+			rbc = Channels.newChannel(website.openStream());
+			FileOutputStream fos = new FileOutputStream(destination);
+			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+			fos.close();
+			rbc.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ unzipper/src/test/java/ivan/polovyi/downloadfileandunzip/unzipper/UnzipperTest.java
@@ -0,0 +1,38 @@
+package ivan.polovyi.downloadfileandunzip.unzipper;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class UnzipperTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public UnzipperTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( UnzipperTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
